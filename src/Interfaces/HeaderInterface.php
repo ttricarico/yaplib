9f7b64980f8fddde4b2e6e5cfcb84c37614beb6a
Thomas <thomas.tricarico@gmail.com>
2013-08-23 16:07:55 -0400

    fixed header problems

--- src/Interfaces/HeaderInterface.php
+++ src/Interfaces/HeaderInterface.php
@@ -1,7 +1,5 @@
 <?php
 
 interface HeaderInterface {
-	public static function sendHTTPResponse($response);
-	public static function sendContentType($type);
-	public static function sendCustomHeader($header);
+	
 }
